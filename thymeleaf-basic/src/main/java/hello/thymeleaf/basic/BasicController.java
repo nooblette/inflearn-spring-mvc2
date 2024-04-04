48edba1ba9924c00a67d38814e8fb4a59318b04f
nooblette <min9hyuk@gmail.com>
2024-04-04 16:37:15 +0900

    feat : sec 01. 타임리프 - 기본 기능 - 기본 객체들

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
 
 @Controller
@@ -49,6 +53,23 @@ public class BasicController {
 		return "basic/variable";
 	}
 
+	@GetMapping("/basic-objects")
+	public String basicObject(Model model, HttpServletRequest request, HttpServletResponse response, HttpSession session){
+		// http request : 유저의 요청에 따른 처리가 완료되면 종료된다.
+		// http session : 사용자가 웹 브라우저를 끄지 않는 이상 유지되는 정보
+		session.setAttribute("sessionData", "Hello Session");
+		model.addAttribute("request", request);
+		model.addAttribute("response", response);
+		model.addAttribute("servletContext", request.getServletContext());
+		return "basic/basic-objects";
+	}
+
+	@Component("helloBean") // HelloBean 클래스를 스프링 빈으로 등록
+	static class HelloBean {
+		public String hello(String data) {
+			return "Hello " + data;
+		}
+	}
 	@Data
 	static class User {
 		private String username;
