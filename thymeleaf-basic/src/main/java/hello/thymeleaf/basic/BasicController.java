aa999753a15848171239a15ba9ea898340f6eddb
nooblette <min9hyuk@gmail.com>
2024-04-04 16:52:52 +0900

    feat : sec 01. 타임리프 - 기본 기능 - 유틸리티 객체와 날짜

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,5 +1,6 @@
 package hello.thymeleaf.basic;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -69,6 +70,12 @@ public class BasicController {
 		public String hello(String data) {
 			return "Hello " + data;
 		}
+	}
+
+	@GetMapping("/date")
+	public String data(Model model){
+		model.addAttribute("localDateTime", LocalDateTime.now());
+		return "basic/date";
 	}
 	@Data
 	static class User {
