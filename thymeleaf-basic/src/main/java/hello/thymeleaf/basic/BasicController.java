6209b5b8a714ca4df4cae7d0a79e4c3e66fc10b1
nooblette <min9hyuk@gmail.com>
2024-04-05 20:04:35 +0900

    feat : sec 01. 타임리프 - 기본 기능 - 반복

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -105,6 +105,23 @@ public class BasicController {
 	public String attribute(){
 		return "basic/attribute";
 	}
+
+	@GetMapping("/each")
+	public String each(Model model){
+		addUsers(model);
+		return "/basic/each";
+	}
+
+	private void addUsers(Model model){
+		List<Object> list = new ArrayList<>();
+		list.add(new User("userA", 10));
+		list.add(new User("userB", 20));
+		list.add(new User("userC", 30));
+
+		model.addAttribute("users", list);
+
+	}
+
 	@Data
 	static class User {
 		private String username;
