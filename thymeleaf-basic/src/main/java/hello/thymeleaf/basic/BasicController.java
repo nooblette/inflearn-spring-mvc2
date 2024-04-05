8af87d583c4582f10b59bffa49875581872a9cbe
nooblette <min9hyuk@gmail.com>
2024-04-05 20:25:29 +0900

    feat : sec 01. 타임리프 - 기본 기능 - 조건부 평가

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -122,6 +122,12 @@ public class BasicController {
 
 	}
 
+	@GetMapping("/condition")
+	public String condition(Model model){
+		addUsers(model);
+		return "basic/condition";
+	}
+
 	@Data
 	static class User {
 		private String username;
