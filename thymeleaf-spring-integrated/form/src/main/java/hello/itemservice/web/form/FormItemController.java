cd11286998e4afe13426cb0f811849c2e56c2755
nooblette <min9hyuk@gmail.com>
2024-04-07 22:51:01 +0900

    feat : sec 02. 타임리프 - 스프링 통합과 폼 - 체크 박스 - 멀티

--- thymeleaf-spring-integrated/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ thymeleaf-spring-integrated/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -10,7 +10,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,6 +21,23 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    /**
+     * @ModelAttribute("regions")
+     * - FormItemController 컨트룰러를 호출하는 경우, @ModelAttribute 어노테이션이 붙어있으면 항상 regions 메서드가 반환하는 값이 model에 들어간다.
+     * - 이때 model의 key 값으로는 @ModelAttribute 애노테이션에 넘긴 값(e.g. regions)으로 설정된다.
+     * */
+    @ModelAttribute("regions") // GET 요청으로 넘어온 값을 객체로 받는 ModelAttribute 기능과는 다른 기능이다.
+    public Map<String, String> regions(){
+        // 일반 HashMap 구현 객체를 사용하는 경우 순서가 보장되지 않아 LinkedHashMap 객체를 사용한다.
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울"); // key(시스템에서 오고가는 값) : "SEOUL", value(사용자에게 보여주는 값) : "서울"
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+
+        // FormItemController 컨트룰러에 요청하면 이 regions 변수는 항상 자동으로 model에 들어간다.
+        return regions;
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -44,6 +63,7 @@ public class FormItemController {
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
