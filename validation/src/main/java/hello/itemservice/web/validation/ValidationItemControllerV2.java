c726380ed5d7eb0f27bff9c948bd2f19ea0e1926
nooblette <min9hyuk@gmail.com>
2024-04-12 08:06:45 +0900

    feat : sec 04. 검증1 - Validation - FieldError, ObjectError

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -50,7 +50,7 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         log.info("call ValidationItemControllerV1.addItemV1()");
         // BindingResult : item 객체의 바인딩한 결과가 담긴다. (validationItemControllerV1 클래스의 errors Map 역할)
@@ -76,6 +76,44 @@ public class ValidationItemControllerV2 {
             if(resultPrice < 10000) {
                 // ObjectError : 특정 필드가 아닌 복합 롤(객체 자체) 에러 처리 객체
                 bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+            }
+        }
+        if(bindingResult.hasErrors()) {
+            // 검증에 실패하면 다시 입력 폼으로 이동한다.
+            log.info("errors = {}", bindingResult);
+            // model.addAttribute("bindingResult", bindingResult); // bindingResult 객체는 자동으로 뷰(View) 파일로 넘어가기 때문에 모델(Model)에 담는 로직은 생략한다.
+            return "validation/v2/addForm";
+        }
+
+        // 검증 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        // 특정 단일 필드 검증
+        if (!StringUtils.hasText(item.getItemName())) {
+            // FieldError : 스프링이 제공하는 필드 단위의 에러 처리 객체
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수 입니다."));
+        }
+
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000){
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000 ~ 1,000,000까지 허용됩니다."));
+        }
+
+        if(item.getQuantity() == null || item.getQuantity() >= 9999){
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null ,"수량은 최대 9,999까지 허용됩니다."));
+        }
+
+        // 특정 단일 필드가 아닌 복합 롤 검증
+        if(item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000) {
+                // ObjectError : 특정 필드가 아닌 복합 롤(객체 자체) 에러 처리 객체
+                bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
             }
         }
         if(bindingResult.hasErrors()) {
