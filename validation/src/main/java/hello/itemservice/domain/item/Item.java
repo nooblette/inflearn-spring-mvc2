f346abd2c4666889bc433c094a8c71b36ad3536c
nooblette <min9hyuk@gmail.com>
2024-04-18 07:20:18 +0900

    feat : sec 05. 검증2 - Bean Validation - 오브젝트 오류

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -7,10 +7,12 @@ import javax.validation.constraints.NotNull;
 
 // Range : 표준이 아님, hibernate 구현체에서만 동작한다.
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import lombok.Data;
 
 @Data
+// @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000 이상이여야합니다.") // 오브젝트 오류는 자바로 작성하는 것을 권장
 public class Item {
 
     private Long id;
