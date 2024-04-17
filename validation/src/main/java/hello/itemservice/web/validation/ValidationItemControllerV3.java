bf7651b02876309262178a2bfbbd5948b73e9ac7
nooblette <min9hyuk@gmail.com>
2024-04-18 07:33:26 +0900

    feat : sec 05. 검증2 - Bean Validation - 수정에 적용

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -57,7 +57,6 @@ public class ValidationItemControllerV3 {
             }
         }
 
-
         if (bindingResult.hasErrors()) {
             // 검증에 실패하면 다시 입력 폼으로 이동한다.
             log.info("errors = {}", bindingResult);
@@ -79,7 +78,20 @@ public class ValidationItemControllerV3 {
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+        //특정 필드 예외가 아닌 전체 예외
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[] {10000, resultPrice}, null);
+            }
+        }
+
+        if(bindingResult.hasErrors()){
+            log.info("errors={}", bindingResult);
+            return "validation/v3/editForm";
+        }
+
         itemRepository.update(itemId, item);
         return "redirect:/validation/v3/items/{itemId}";
     }
