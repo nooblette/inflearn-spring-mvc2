ac98af261f558b2aabd51bac67cb674ca709671c
nooblette <min9hyuk@gmail.com>
2024-05-07 22:47:53 +0900

    feat : sec 06. 로그인 처리1 - 쿠키, 세션 - 로그인 처리하기 - 쿠키 사용

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,43 @@
 package hello.login.web;
 
+import javax.servlet.http.HttpServletRequest;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final MemberRepository memberRepository;
 
-    @GetMapping("/")
+    //@GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name="memberId", required = false /* 로그인하지 않은 사용자도 Home 화면엔 접속할 수 있어야 한다.*/)
+                                Long memberId, Model model) {
+        if(memberId == null) { // 로그인하지 않은 사용자
+            return "home";
+        }
+
+        // 로그인한 회원에게는 회원 이름을 보여주는 기능을 추가한다.
+
+        // 쿠키의 값으로 해당하는 회원(Member)가 없다면 Home 화면을 노출한다.
+        Member loginMember = memberRepository.findById(memberId);
+        if(loginMember == null){
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -1,5 +1,7 @@
 package hello.login.web.login;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -25,7 +27,9 @@ public class LoginController {
 	}
 
 	@PostMapping("/login")
-	public String login(@Valid @ModelAttribute("loginForm")LoginForm form, BindingResult bindingResult){
+	public String login(@Valid @ModelAttribute("loginForm")LoginForm form,
+						BindingResult bindingResult,
+						HttpServletResponse httpServletResponse){
 		if(bindingResult.hasErrors()){
 			return "login/loginForm";
 		}
@@ -38,7 +42,25 @@ public class LoginController {
 			return "login/loginForm";
 		}
 
-		// TODO 로그인 성공 처리
+		// 로그인 성공 처리 - 쿠키를 생성한다.
+		// 쿠키에 시간 정보를 주지 않으면 브라우저 종료시 만료된다(세션 쿠키)
+		Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId())); // 쿠키 이름 : memberId, 쿠키 값 : 회원 id(loginId 아님)
+
+		// 클라이언트에게 HTTP 응답을 내려줄때 set-cookies 헤더에 idCookie를 함께 내려준다.
+		httpServletResponse.addCookie(idCookie); // 클라이언트는 서버로 요청을 전송할때 이 쿠키 정보를 항상 포함해서 전송한다.
 		return "redirect:/";
+	}
+
+	@PostMapping("/logout")
+	public String logout(HttpServletResponse httpServletResponse){
+		expireCookie(httpServletResponse, "memberId");
+		return "redirect:/";
+	}
+
+	private static void expireCookie(HttpServletResponse httpServletResponse, String cookieName) {
+		// 로그아웃시에는 쿠키를 지운다. (정확히 말하면 시간을 없애서 유효하지 않게 만든다)
+		Cookie expiredCookie = new Cookie(cookieName, null);
+		expiredCookie.setMaxAge(0);
+		httpServletResponse.addCookie(expiredCookie);
 	}
 }
