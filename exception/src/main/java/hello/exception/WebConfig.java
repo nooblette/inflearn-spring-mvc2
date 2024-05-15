6774c916e41f7f0480f8080d03479f0cbf9948e7
nooblette <min9hyuk@gmail.com>
2024-05-15 18:47:43 +0900

    feat : sec 08. 예외 처리와 오류 페이지 - 서블릿 예외 처리 - 인터셉터

--- exception/src/main/java/hello/exception/WebConfig.java
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -3,15 +3,17 @@ package hello.exception;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	@Bean
+	//@Bean
 	public FilterRegistrationBean<Filter> logFilter() {
 		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
 		filterRegistrationBean.setFilter(new LogFilter());
@@ -22,5 +24,14 @@ public class WebConfig implements WebMvcConfigurer {
 		filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
 
 		return filterRegistrationBean;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// 스프링 인터셉터를 사용하는 경우 DispatcherType으로 적용 여부를 구분하지 않고 excludePathPatterns을 활용한다.
+		registry.addInterceptor(new LogInterceptor())
+			.order(1)
+			.addPathPatterns("/**")
+			.excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**"); // /error-page/** 요청에 대해서는 LogInterceptor를 적용하지 않는다.
 	}
 }
---
+++ exception/src/main/java/hello/exception/interceptor/LogInterceptor.java
@@ -0,0 +1,41 @@
+package hello.exception.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+	public static final String LOG_ID = "logId";
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+		String requestURI = request.getRequestURI();
+		String uuid = UUID.randomUUID().toString();
+		request.setAttribute(LOG_ID, uuid);
+		log.info("preHandle");
+		log.info("REQUEST  [{}][{}][{}][{}]", uuid, request.getDispatcherType(), requestURI, handler);
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+						   ModelAndView modelAndView){
+		// 컨트룰러에서 예외가 발생한 경우 인터셉터의 postHandle 메서드는 호출되지 않는다.
+		log.info("postHandle [{}]", modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+								Exception ex) {
+		String requestURI = request.getRequestURI();
+		String logId = (String)request.getAttribute(LOG_ID);
+		log.info("RESPONSE [{}][{}][{}]", logId, request.getDispatcherType(), requestURI);
+		if (ex != null) {
+			log.error("afterCompletion error!!", ex);
+		}
+	}
+}
