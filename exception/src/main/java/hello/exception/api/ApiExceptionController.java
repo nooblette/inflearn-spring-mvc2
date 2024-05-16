10a5ace742483bb70681f2ce670b424aaafb7269
nooblette <min9hyuk@gmail.com>
2024-05-17 08:09:24 +0900

    feat : sec 09. API 예외 처리 - 스프링이 제공하는 ExceptionResolver2

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -3,6 +3,8 @@ package hello.exception.api;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -42,6 +44,11 @@ public class ApiExceptionController {
 	@GetMapping("/api/response-status-ex2")
 	public String responseStatusEx2() {
 		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalStateException());
+	}
+
+	@GetMapping("/api/default-handler-ex")
+	public String defaultHandlerEx(@RequestParam Integer data) {
+		return "ok";
 	}
 
 	@Data
