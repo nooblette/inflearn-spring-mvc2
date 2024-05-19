39a448768f09ae13c42a40efe6e54d81f85cac75
nooblette <min9hyuk@gmail.com>
2024-05-20 07:03:00 +0900

    feat : sec 10. 스프링 타입 컨버터 - 포맷터 적용하기

--- typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -8,6 +8,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConvertor;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -15,9 +16,13 @@ public class WebConfig implements WebMvcConfigurer {
 	// Formatters : Converter의 조금 더 확장된 기능(뒤에서 자세히 설명)
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
-		registry.addConverter(new StringToIntegerConvertor());
-		registry.addConverter(new IntegerToStringConverter());
+		// 주석 처리 (동일한 타입(숫자 <-> 문자) 변환시 컨버터가 우선순위가 더 높기 때문)
+		// registry.addConverter(new StringToIntegerConvertor());
+		// registry.addConverter(new IntegerToStringConverter());
 		registry.addConverter(new StringToIpPortConverter());
 		registry.addConverter(new IpPortToStringConverter());
+
+		// 포맷터 추가
+		registry.addFormatter(new MyNumberFormatter());
 	}
 }
